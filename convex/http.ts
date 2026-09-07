a8c81e596218ee62128195e15fe3462639afcd65
Sang Truong <45520864+sangtrx@users.noreply.github.com>
2026-09-07 13:03:52 +0700

    feat: mount AgentMail webhook

---
+++ convex/http.ts
@@ -0,0 +1,15 @@
+import { AgentMail } from "@agentmail/convex";
+import { httpRouter } from "convex/server";
+import { components } from "./_generated/api";
+import { httpAction } from "./_generated/server";
+
+const agentmail = new AgentMail(components.agentmail);
+const http = httpRouter();
+
+http.route({
+  path: "/agentmail/webhook",
+  method: "POST",
+  handler: httpAction(async (ctx, request) => agentmail.handleWebhook(ctx, request)),
+});
+
+export default http;
